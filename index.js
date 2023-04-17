b69adf46c938c3b8a0a2f687ec7575b2c4231bae
Soniya Jeena <114814620+SoniyaJeena30@users.noreply.github.com>
2023-04-18 00:56:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,111 @@
+let movies = [
+  {
+    name: "Brahmastra",
+    des:
+      "when a group passengers get stuck in a landslide, a single mother struggles to protect her son and secure his oxygen cylinder from a desperate officer.",
+    image: "images/slider 1.png"
+  },
+  {
+    name: "luka",
+    des:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, fugiat quam nesciunt eligendi dolorum totam.",
+    image: "images/slider 2.png"
+  },
+  {
+    name: "O2",
+    des:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, fugiat quam nesciunt eligendi dolorum totam.",
+    image: "images/slider 3.png"
+  },
+  {
+    name: "loki",
+    des:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, fugiat quam nesciunt eligendi dolorum totam.",
+    image: "images/slider 4.png"
+  }
+];
+
+const carousel = document.querySelector(".carousel");
+let sliders = [];
+
+let slideIndex = 0; //track the current slide
+
+const createSlide = () => {
+  if (slideIndex >= movies.length) {
+    slideIndex = 0;
+  }
+
+  //creating DOM element
+  let slide = document.createElement("div");
+  let imgElement = document.createElement("img");
+  let content = document.createElement("div");
+  let h1 = document.createElement("h1");
+  let p = document.createElement("p");
+
+  //attaching all elements
+  imgElement.appendChild(document.createTextNode(""));
+  h1.appendChild(document.createTextNode(movies[slideIndex].name));
+  p.appendChild(document.createTextNode(movies[slideIndex].des));
+  content.appendChild(h1);
+  content.appendChild(p);
+  slide.appendChild(content);
+  slide.appendChild(imgElement);
+  carousel.appendChild(slide);
+
+  //setting up images
+  imgElement.src = movies[slideIndex].image;
+  slideIndex++;
+
+  //setting elements classname
+  slide.className = "slider";
+  content.className = "slide-content";
+  h1.className = "movie-tittle";
+  p.className = "movie-des";
+
+  sliders.push(slide);
+
+  //adding sliding effect
+
+  if (sliders.length) {
+    sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${
+      30 * (sliders.length - 2)
+    }px)`;
+  }
+};
+for (let i = 0; i < 3; i++) {
+  createSlide();
+}
+setInterval(() => {
+  createSlide();
+}, 3000);
+
+const videoCards = [...document.querySelectorAll(".video-card")];
+
+videoCards.forEach((item) => {
+  item.addEventListener("mousecover", () => {
+    let video = item.children[1];
+    video.play();
+  });
+  item.addEventListener("mouseleave", () => {
+    let video = item.children[1];
+    video.pause();
+  });
+});
+
+//card sliders
+let cardContainers = [...document.querySelectorAll(".card-container")];
+let preBtns = [...document.querySelectorAll(".pre-btn")];
+let nxtBtns = [...document.querySelectorAll(".nxt-btn")];
+
+cardContainers.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtns[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth - 200;
+  });
+
+  preBtns[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth + 200;
+  });
+});
